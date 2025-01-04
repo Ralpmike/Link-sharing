012a44a516de8264f5d4e652c4b7be80a250dc0b
Ralpmike <ralphmike996@gmail.com>
2025-01-04 14:13:57 +0100

    set up shadcn ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
+import clsx from 'clsx'
 import './App.css'
 
 function App() {
 
   return (
 
-    <div>
+    <div className={clsx('App', 'bg-gray-100', 'text-gray-800 font-bold text-2xl')}>
       <h1>Vite + React</h1>
     </div>
   )
